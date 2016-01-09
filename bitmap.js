4a9f0fb7f4328f866526cf165fb67fcb323265fe
ricecreamdude <ho.joshua4@gmail.com>
2016-01-08 16:48:28 -0800

    bitmap translator, added comments, no functionality changed

--- bitmap.js
+++ bitmap.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-
 var bitmap = fs.readFileSync(process.argv[2]);
 
 console.log(`type: ${bitmap.toString('utf-8', 0, 2)}`);
@@ -9,7 +8,6 @@ console.log(`start of pixel data: ${bitmap.readUInt32LE(10)}`)
 console.log(`width: ${bitmap.readUInt32LE(18)}`)
 console.log(`height: ${bitmap.readUInt32LE(22)}`);
 console.log(`number of colors: ${bitmap.readUInt32LE(46)}`);
-
 
 var colorArray = (function() {
 
@@ -21,8 +19,11 @@ var colorArray = (function() {
     }
   }
   return toReturn;
+
 })();
 
+//Begin loading image data into Javascript arrays, starting at byte 54 (because headers and stuff)
+//We have verified that byte 54 is indeed where the image starts - this image does NOT have a color pallete.
 
 var colorPalette = createRgb(colorArray);
 
@@ -31,25 +32,15 @@ for(var i = 0; i < bitmap.length; i++){
 		bitmap[i] = colorPalette[i];
 	}
 
+  //Writes bitmap to greyscale.bmp (All data is done processing by here)
 	if(i === bitmap.length-1) {
 		console.log('written');
 		writeFile(bitmap, {fileToCreate: 'greyscale.bmp'});
 	}
 }
 
-
-
-
-
-
-// Buffer is the buffer that will be written to file
-// Options are the write options
-function writeFile(buffer, options) {
-  var writeStream = fs.createWriteStream(options.fileToCreate);
-  writeStream.write(buffer);
-  writeStream.end();
-}
-
+//Calls image data from coloArray 
+//Returns an array
 function createRgb(array) {
   var colorPallete = [];
   for (var i = 0; i < array.length; i = i + 3) {
@@ -59,8 +50,20 @@ function createRgb(array) {
   return colorPallete;
 }
 
+//Converts an RGB set into grayscale via averaging
+//Returns a gray color
 function grayScale(r,g,b){
 	var toReturn = (r+b+g)/3;
 	toReturn = Math.floor(toReturn);
 	return toReturn;
+}
+
+// Buffer is the buffer that will be written to file
+// Options are the write options
+//CLEAR
+
+function writeFile(buffer, options) {
+  var writeStream = fs.createWriteStream(options.fileToCreate);
+  writeStream.write(buffer);
+  writeStream.end();
 }
